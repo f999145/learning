67b93d0ab2f8c887f5c2f06baa4ac77d64722f5c
f999145 <f999145@yandex.ru>
2022-12-17 00:23:30 +0300

    test

--- ffmpeg/cycle_h264.py
+++ ffmpeg/cycle_h264.py
@@ -30,18 +30,13 @@ for file in files[:1]:
         
         # '-r', '24',             # frame rate
         
-        '-c:v', 'dnxhd',        # FFmpeg contains two ProRes encoders, the prores-aw and prores-ks encoder.
-                                # You can choose the encoder you want using the -vcodec option.
-        
-        '-profile:v', 'dnxhr_sq',  
-                                    # DNxHR LB: dnxhr_lb - Low Bandwidth. 8-bit 4:2:2 (yuv422p). Offline Quality.
-                                    # DNxHR SQ: dnxhr_sq - Standard Quality. 8-bit 4:2:2 (yuv422p). Suitable for delivery format.
-                                    # DNxHR HQ: dnxhr_hq - High Quality. 8-bit 4:2:2 (yuv422p).
-                                    # DNxHR HQX: dnxhr_hqx - High Quality. 10-bit 4:2:2 (yuv422p10le). UHD/4K Broadcast-quality delivery.
-                                    # DNxHR 444: dnxhr_444 - Finishing Quality. 10-bit 4:4:4 (yuv444p10le). Cinema-quality delivery.
-        
-        # '-c:a', 'aac', '-b:a', '192k', # кодирование аудио
-        '-c:a', 'pcm_s16le', # 'pcm_s32le'
+        '-c:v', 'h264_nvenc',
+        '-b:v', '40M',
+        '-minrate', '39M',
+        '-maxrate', '40M',
+        '-bufsize', '2M',
+       
+        '-c:a', 'aac', '-b:a', '192k', # кодирование аудио
         
         '-alpha_bits', '0', # Possible values are 0, 8 and 16. Use 0 to disable alpha plane coding.
         
