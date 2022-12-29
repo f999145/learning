2ba38fa22c79126812c26223e9555e060993a529
f999145 <f999145@yandex.ru>
2022-12-29 18:37:58 +0300

    update

--- ffmpeg/cycle copy.bat
+++
@@ -1,3 +0,0 @@
-mkdir outputs
-for %%a in ("*.mkv") do ffmpeg -y -i "%%a" -vf scale=-1:720 -preset slow -c:v h264_nvenc -b:v 3M -profile:v high -level 4 -c:a aac -b:a 192k "outputs\%%~na.mp4"
-pause
