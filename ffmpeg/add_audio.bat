17ebaafe53ddb9f703185ae4feac6289dac9039b
f999145 <f999145@yandex.ru>
2023-01-20 12:49:19 +0300

    update

--- ffmpeg/add_audio.bat
+++ ffmpeg/add_audio.bat
@@ -1,1 +1,2 @@
-ffmpeg -i "How to install mysql and phpmyadmin.webm" -i audio.mp3 -map 0 -map 1:a -c:v copy -shortest output.webm
+ffmpeg -i "lou.mkv" -i Untitled.wav -map 0 -map 1:a -c:v copy output.mkv
+pause
