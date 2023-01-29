66fbc8759c8a4ba699e99345d25c0048ba1dc082
f999145 <f999145@yandex.ru>
2023-01-29 17:27:43 +0300

    update

--- ffmpeg/cycle mp4 720.bat
+++ ffmpeg/cycle mp4 720.bat
@@ -1,3 +1,3 @@
 mkdir outputs
-for %%a in ("*.webm") do ffmpeg -y -i "%%a" -vf scale=1280:-1 -preset slow -c:v h264_nvenc -b:v 3M -profile:v high -level 4 -c:a aac -b:a 192k "outputs\%%~na.mp4"
+for %%a in ("*.mkv") do ffmpeg -y -i "%%a" -vf scale=1280:-1 -preset slow -c:v h264_nvenc -b:v 3M -profile:v high -level 4 -af "pan=stereo|FL < 0.7*FC + 0.3*FLC + 0.3*FL + 0.3*BL + 0.3*SL + 0.5*LFE | FR < 0.7*FC + 0.3*FRC + 0.3*FR + 0.3*BR + 0.3*SR + 0.5*LFE" -c:a aac -b:a 192k "outputs\%%~na.mp4"
 pause
