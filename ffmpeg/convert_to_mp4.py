4187474320d6b5c9f97859ddfc5eb7a19cae4c18
f999145 <f999145@yandex.ru>
2023-05-17 22:45:34 +0300

    update

---
+++ ffmpeg/convert_to_mp4.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import subprocess
+from textwrap import shorten
+
+# path_to_folder = Path('a:\\')
+path_to_folder = Path.cwd()
+file_list = list(path_to_folder.iterdir())
+print()
+for index, item in enumerate(file_list):
+    print(f'{index:02} {item.name}')
+print('-'*80)
+while True:
+    try:
+        number = int(input('\nВведите номер файла: '))
+        break
+    except ValueError:
+        while True:
+            match input('не число, повторить? (y/n):'):
+                case 'y':
+                    break
+                case 'n':
+                    exit()
+                case _:
+                    continue
+            
+    
+video = file_list[number]
+
+print(f'\nvideo file:\n\t{video.name}')
+
+name = video.name.replace(video.suffix, '')
+symbols = ' abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789'
+name = ''.join(filter(lambda x: x if x.lower() in symbols else '', name))
+
+out_name = shorten(name, 40, placeholder='').replace(' ', '_') + '_out' + '.mp4'
+out_file = path_to_folder.joinpath(out_name)
+print('-'*80)
+print(f'\nout file: \n\t{out_file}')
+
+ 
+if not input('\nПродолжить? (enter)') == '':
+    exit()
+
+args = [
+    'ffmpeg', '-y',
+    '-i', video,
+    
+    '-map', '0:v',
+    '-map', '0:a',
+    '-map', '0:s',
+    
+    '-vf', 'scale=1920:-1',
+    '-c:v', 'h264_nvenc',
+    '-preset', 'fast',
+    '-profile:v', 'main',
+    '-level', '4.2',
+    '-cbr', 'True',
+    '-b:v', '8M',
+    
+    '-c:a', 'ac3',
+    '-af', "pan=stereo|FL < 0.7*FC + 0.3*FLC + 0.3*FL + 0.3*BL + 0.3*SL + 0.5*LFE | FR < 0.7*FC + 0.3*FRC + 0.3*FR + 0.3*BR + 0.3*SR + 0.5*LFE",
+    
+    '-c:s', 'mov_text',
+    out_file
+]
+subprocess.run(args, shell=True, check=True)
--- ffmpeg/cycle.bat
+++ ffmpeg/cycle.bat
@@ -1,3 +1,24 @@
-mkdir outputs
-for %%a in ("*.webm") do ffmpeg -y -i "%%a" -vf scale=-1:1080 -c:v dnxhd -profile:v dnxhr_lb -c:a aac -b:a 192k -alpha_bits 0 "outputs\%%~na.mov"
+@echo off
+setlocal enabledelayedexpansion
+
+set input_folder=A:\input_folder
+set output_folder=A:\output_folder
+
+for %%a in ("%input_folder%\*.*") do (
+    set input_file=%%a
+    set output_file=%output_folder%/%%~na.mp4
+    ffmpeg -y ^
+    -i "!input_file!" ^
+    -vf scale=1920:-1 ^
+    -c:v h264_nvenc ^
+    -preset fast ^
+    -profile:v main ^
+    -level 4.2 ^
+    -cbr true ^
+    -b:v 8M ^
+    -c:a aac ^
+    -b:a 192k ^
+    -af "pan=stereo|FL < 0.7*FC + 0.3*FLC + 0.3*FL + 0.3*BL + 0.3*SL + 0.5*LFE | FR < 0.7*FC + 0.3*FRC + 0.3*FR + 0.3*BR + 0.3*SR + 0.5*LFE" ^
+    "!output_file!"
+)
 pause
