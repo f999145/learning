998da989d4d87e8c80224c3a4940d51897b435b9
f999145 <f999145@yandex.ru>
2024-01-23 13:20:25 +0300

    update

--- streamlit/main.py
+++ streamlit/main.py
@@ -19,19 +19,22 @@ def main():
     df = load_data()
     df1 = df.query('index < @val')
     # sl.sidebar.write('test')
-    # fig = plt.figure(figsize=(8,6))
-    # plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
+    fig = plt.figure(figsize=(8,4))
+    plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
     # sl.write(load_style())
     # plt.style.use(load_style())
     
-    # plt.plot(df1.index, df1['dwell-time'])
-    # sl.write(fig)
+    plt.plot(df1.index, df1['dwell-time'])
+    col1, col2 = sl.columns((3,3))
+    col1.write(fig)
+    col2.line_chart(data=df1,y='dwell-time',)
+    sl.write(fig)
     sl.line_chart(data=df1,y='dwell-time',)
     
     slice_ = ['sum']
     tmp = df1.groupby('site').agg(sum=('dwell-time','sum'), mean=('dwell-time','mean'), median=('dwell-time','median'))
-    tmp = tmp.style.set_properties(**{'background-color': '#2b1212'}, subset=slice_)
-    sl.dataframe(tmp)
+    # tmp = tmp.style.set_properties(**{'background-color': '#2b1212'}, subset=slice_)
+    sl.table(tmp)
 
 
     
